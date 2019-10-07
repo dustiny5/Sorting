91d2a6789b7b4d4b97baaf13eba0f1749bc42f87
dustiny5 <dustiny5@users.noreply.github.com>
2019-10-07 16:51:29 -0700

    Added while loop to bubble_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -22,24 +22,35 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    start = arr[0]
-    for i in range(1, len(arr)):
-        if start > arr[i]:
-            # Get temp value
-            temp = start
+    while True:
+        if arr == []:
+            return arr
             
-            # Change to current 
-            start = arr[i]
-            
-            # Swap numbers in list
-            arr[i] = temp
-            arr[i-1] = start
-            
-            # Change start to swapped number
-            start = arr[i]
+        start = arr[0]
+        swap = 0
+        for i in range(1, len(arr)):
+            if start > arr[i]:
+                # Get temp value
+                temp = start
 
-        elif start < arr[i]:
-            start = arr[i]
+                # Change to current 
+                start = arr[i]
+
+                # Swap numbers in list
+                arr[i] = temp
+                arr[i-1] = start
+
+                # Change start to swapped number
+                start = arr[i]
+
+                swap += 1
+
+            elif start < arr[i]:
+                start = arr[i]
+
+        if swap == 0:
+            break
+
     return arr
 
 
