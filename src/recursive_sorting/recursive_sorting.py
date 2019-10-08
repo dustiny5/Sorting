c8d18902e86b57c6cb5aee47838c3f2d9559a460
dustiny5 <dustiny5@users.noreply.github.com>
2019-10-08 16:32:05 -0700

    Got rid of for loop and concatenate list instead

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -17,16 +17,12 @@ def merge( arrA, arrB ):
             arrA.remove(arrA[0])
 
         elif len(arrA) > 1 and len(arrB) == 0:
-            for el in arrA:
-                merged_arr.append(el)
-                arrA.remove(el)
-                break
+            merged_arr += arrA
+            break
 
         elif len(arrA) == 0 and len(arrB) > 1:
-            for it in arrB:
-                merged_arr.append(it)
-                arrB.remove(it)
-                break
+            merged_arr += arrB
+            break
 
         elif arrA[0]>arrB[0]:
             merged_arr.append(arrB[0])
